b18d9510d3cf4b1a34e6676fa2639db3f637ce9a
Rafael Vieira <rafaellvs322@gmail.com>
2022-01-12 17:19:47 -0300

    ENH: update app index entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import WebFont from 'webfontloader'
+
+import Router from 'src/components/Router'
+import GlobalStyles from 'src/helpers/global-styles'
+
+WebFont.load({
+  google: {
+    families: ['Montserrat:300,500,700,800']
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
