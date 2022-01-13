4d7c619647cdfe2eb3f181e0a0c9e9abe1294659
Rafael Vieira <rafaellvs322@gmail.com>
2022-01-13 15:12:30 -0300

    FEA: add app theme

---
+++ src/helpers/theme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  primary: '#08012A',
+  secondary: '#78D98A',
+  white: '#FFFFFF',
+  black: '#08012A',
+  yellow: '#F0C808',
+  red: '#E1341E',
+  darkRed: '#871F12',
+  gray: '#565959'
+}
+
+const navbarHeight = '90px'
+
+export default { colors, navbarHeight }
