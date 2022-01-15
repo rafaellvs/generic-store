7c6027d38a9763a3a0ec8b24b5ff176c7badedd8
Rafael Vieira <rafaellvs322@gmail.com>
2022-01-15 18:55:29 -0300

    FEA: add links to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 import logo from 'src/assets/images/logo.png'
 
-import { Container, Logo } from './styled'
+import CartContext from 'src/helpers/cart-context'
+
+import Link from 'src/components/core/Link'
+
+import {
+  Container,
+  Logo,
+  Links,
+  CartLink
+} from './styled'
 
 const Header = () => {
+  const { cartProducts } = useContext(CartContext)
+  const totalProductsAtCart = cartProducts.length
+
   return (
     <Container>
-      <Logo src={logo} alt='Liven Logo' />
+      <Link to='/'>
+        <Logo src={logo} alt='Liven Logo' />
+      </Link>
+
+      <Links>
+        <Link to='/shopping-list'>Shopping List</Link>
+
+        <CartLink to='/cart'>
+          <AiOutlineShoppingCart />
+          {`Cart (${totalProductsAtCart})`}
+        </CartLink>
+      </Links>
     </Container>
   )
 }
