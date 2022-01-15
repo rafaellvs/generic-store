22377819a197f12c5e90541c67a217d2fcc7f9bb
Rafael Vieira <rafaellvs322@gmail.com>
2022-01-15 18:41:44 -0300

    ENH: update theme with new colors

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,12 +1,15 @@
 const colors = {
   primary: '#08012A',
   secondary: '#78D98A',
+  secondaryHover: '#2D9F42',
+  success: '#27D844',
   white: '#FFFFFF',
   black: '#08012A',
   yellow: '#F0C808',
   red: '#E1341E',
   darkRed: '#871F12',
-  gray: '#565959'
+  gray: '#565959',
+  lightGray: '#929696'
 }
 
 const navbarHeight = '90px'
